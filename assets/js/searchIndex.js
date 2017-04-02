3be630c74ce7ca93dfd1acd94a5aa37ca05cb000
Gary Ewan Park <gep13@gep13.co.uk>
2017-04-02 20:54:29 +0000

    AppVeyor Publish: b5e968e0f88a134fb56d6ca506e0c4e4965f43da Merge branch 'release/0.3.0'
    * release/0.3.0:
      (GH-7) Added Wyam Docs
      (GH-6) Updated project readme
      (GH-6) Added missing packages.config file
      (GH-6) Added GRM config file
      (GH-6) Removed GitVersion Config file
      (GH-6) Added SolutionInfo.cs
      (GH-6) Added folder conventions
      (GH-6) Added setup.cake file
      (GH-6) Updated build.ps1 file
      (GH-6) Updated gitignore file
      (GH-6) Updated AppVeyor File
      (GH-6) Renamed AppVeyor File

---
+++ assets/js/searchIndex.js
@@ -0,0 +1,210 @@
+
+var camelCaseTokenizer = function (obj) {
+    var previous = '';
+    return obj.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
+        var current = cur.toLowerCase();
+        if(acc.length === 0) {
+            previous = current;
+            return acc.concat(current);
+        }
+        previous = previous.concat(current);
+        return acc.concat([current, previous]);
+    }, []);
+}
+lunr.tokenizer.registerFunction(camelCaseTokenizer, 'camelCaseTokenizer')
+var searchModule = function() {
+    var idMap = [];
+    function y(e) { 
+        idMap.push(e); 
+    }
+    var idx = lunr(function() {
+        this.field('title', { boost: 10 });
+        this.field('content');
+        this.field('description', { boost: 5 });
+        this.field('tags', { boost: 50 });
+        this.ref('id');
+        this.tokenizer(camelCaseTokenizer);
+
+        this.pipeline.remove(lunr.stopWordFilter);
+        this.pipeline.remove(lunr.stemmer);
+    });
+    function a(e) { 
+        idx.add(e); 
+    }
+
+    a({
+        id:0,
+        title:"BaseNewAndUpdateApplicationSettings",
+        content:"BaseNewAndUpdateApplicationSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:1,
+        title:"BaseNewAndUpdateDeploymentSettings",
+        content:"BaseNewAndUpdateDeploymentSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:2,
+        title:"NewDeploymentSettings",
+        content:"NewDeploymentSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:3,
+        title:"MageAliases",
+        content:"MageAliases",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:4,
+        title:"TrustLevel",
+        content:"TrustLevel",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:5,
+        title:"BaseNewAndUpdateMageSettings",
+        content:"BaseNewAndUpdateMageSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:6,
+        title:"SignSettings",
+        content:"SignSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:7,
+        title:"Algorithm",
+        content:"Algorithm",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:8,
+        title:"Processor",
+        content:"Processor",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:9,
+        title:"UpdateApplicationSettings",
+        content:"UpdateApplicationSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:10,
+        title:"UpdateDeploymentSettings",
+        content:"UpdateDeploymentSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:11,
+        title:"NewApplicationSettings",
+        content:"NewApplicationSettings",
+        description:'',
+        tags:''
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/BaseNewAndUpdateApplicationSettings',
+        title:"BaseNewAndUpdateApplicationSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/BaseNewAndUpdateDeploymentSettings',
+        title:"BaseNewAndUpdateDeploymentSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/NewDeploymentSettings',
+        title:"NewDeploymentSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/MageAliases',
+        title:"MageAliases",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/TrustLevel',
+        title:"TrustLevel",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/BaseNewAndUpdateMageSettings',
+        title:"BaseNewAndUpdateMageSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/SignSettings',
+        title:"SignSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/Algorithm',
+        title:"Algorithm",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/Processor',
+        title:"Processor",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/UpdateApplicationSettings',
+        title:"UpdateApplicationSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/UpdateDeploymentSettings',
+        title:"UpdateDeploymentSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Mage/Cake.Mage/api/Cake.Mage/NewApplicationSettings',
+        title:"NewApplicationSettings",
+        description:""
+    });
+
+    return {
+        search: function(q) {
+            return idx.search(q).map(function(i) {
+                return idMap[i.ref];
+            });
+        }
+    };
+}();
